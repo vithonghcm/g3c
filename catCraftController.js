6a026ccfa7d5174897e46b4c13976e53052c3f47
Huu Loc Tong <1453023@student.hcmus.edu.vn>
2016-10-27 23:52:22 +0700

    undo revert

--- catCraftController.js
+++
@@ -1,9 +0,0 @@
-var catCraftController = angular.module('catCraftController',[])
-	.controller('getCatCraftController', function($scope){
-		var getCatCraftController = this;
-		getCatCraftController.categories =
-		[
-			{name:'name1', description:'des1'},
-			{name:'name2', description:'des2'}
-		]
-	});
