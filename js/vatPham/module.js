b134ef53a66bfeb8d4974b6bdcfa98c6ec2a3eb6
tranthuhuong <thuhuong.tran204@gmail.com>
2016-11-11 10:12:17 +0700

    VatPham

---
+++ js/vatPham/module.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* Blog Module */
+
+angular.module('g3cApp.vatPham', [])
+	.config(['$routeProvider', function config( $routeProvider ) {
+	    $routeProvider.when('/vatPham', {
+	        controller: 'vatPhamController',
+	        templateUrl: 'html/vatPham/vatPham.html'
+	    }).when('/vatPham/:idVatPham', {
+	        controller: 'vatPhamChiTietController',
+
+	        templateUrl: 'html/vatPham/chiTietSanPham.html'
+	    });
+	}]);
+	
+	
