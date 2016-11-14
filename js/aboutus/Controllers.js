05cfe8e01ae1f1754e9f44797c2842d366301372
vithonghcm <vithonghcm@gmail.com>
2016-11-14 16:07:32 +0700

    about us

--- js/aboutus/Controllers.js
+++ js/aboutus/Controllers.js
@@ -2,7 +2,7 @@ angular.module('g3cApp.aboutUs')
 	.controller('aboutUsCtrl', function congDongCtrl($scope, $http){
 		$http.get('/data/aboutUs/aboutUs.json')
 		.then( function (respone) {
-			$scope.teamMemberList = respone.data.g3cTeamMemberList;
+			$scope.teamMemberList = respone.data.g3cTeamList;
 		});
 		
 	});
