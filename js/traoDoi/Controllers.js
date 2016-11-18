e9867976f5dc5cc1cc7243b2d05d9a107cfff10b
thuydha <dohuynhanhthuy@gmai.com>
2016-11-18 15:31:58 +0700

    trang xac nhan trao doi

--- js/traoDoi/Controllers.js
+++ js/traoDoi/Controllers.js
@@ -4,12 +4,27 @@ angular.module('g3cApp.traoDoi')
 	.controller('traoDoiCtrl', function traoDoiCtrl($scope, $http){
 		$http.get('../../data/traoDoi/vatPhamTraoDoi.json')
 		.then(function Success(response){
-			$scope.vpTraoDoi = response.data.vatPhamCuaToi;
+			$scope.vpCanTraoDoi = response.data.vatPhamCuaToi;
+			$scope.vpDcTraoDoi = response.data.vatPhamCuaBan;
 		},
 		function Error(response){
 			console.log(response);
 		});
 
+		// var listSP =[];
+		// $scope.addToCart = function (){
+		// 	console.log('add');
+		// 	console.log(VP.tenVP);
+		// 	angular.forEach(listSP, function(addSP){
+		// 		if($scope.VP.tenVP==addSP.ten){
+		// 			addSP.soLuong += 1;
+		// 		}
+		// 		else{
+		// 			$scope.listSP.push({ten:$scope.VP.tenVP, soLuong:'1'})
+		// 		}
+		// 	});
+		// }
+		// console.log(listSP);
 	})
 	.controller('getCatCraftController', function($scope){
 		var getCatCraftController = this;
