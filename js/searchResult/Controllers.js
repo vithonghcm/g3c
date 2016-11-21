8538164482e3cca8fbe79710a2e82cd30b1a46d5
vithonghcm <vithonghcm@gmail.com>
2016-11-21 16:16:22 +0700

    misc change

--- js/searchResult/Controllers.js
+++ js/searchResult/Controllers.js
@@ -1,10 +1,9 @@
 angular.module('g3cApp.searchResult')
 	.controller('searchResultCtrl', function ($scope, $http, $routeParams, $location){
 		$scope.search = function(){
-			var view = '/searchResult';
+			var view = '/searchResult/'+$scope.searchInput;
 			$location.path(view);
-			console.log($location.path);
-		}
-		
+			//console.log($location.path);
+		};
 		
 	});
