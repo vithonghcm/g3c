f26c4f786b5f26db4f61e995fcf5f531b766e762
vithonghcm <vithonghcm@gmail.com>
2016-11-28 08:06:49 +0700

    Revert "Revert "misc change""
    This reverts commit d0b50ef370c8d04862fd280f6a210458131a2ff5.

--- public/js/searchResult/module.js
+++ public/js/searchResult/module.js
@@ -2,7 +2,7 @@
 var app = angular.module('g3cApp.searchResult', ['ngRoute']);
 
 app.config(['$routeProvider', function config($routeProvider){
-	$routeProvider.when('/searchResult',{
+	$routeProvider.when('/searchResult/:searchInput',{
 			controller:'searchResultCtrl',
 			templateUrl:'html/searchResult/searchResult.html'
 		});
