5d2dc8cbcdbea29b320482b4d776417af3c63140
vithonghcm <vithonghcm@gmail.com>
2016-12-07 15:06:36 +0700

    misc change

---
+++ public/js/header/script.js
@@ -0,0 +1,14 @@
+/*
+$(document).ready(function() {
+	$('#search-open').on('click',function(event){
+		alert('aa');
+		//$("#g3c-search-container").toggleClass('g3c-search-open');
+	});
+});
+*/
+$('body').on('click', '#search-open', function () { 
+	$("#g3c-search-container").toggleClass('g3c-search-open');
+});
+	
+
+
