36a4ba08b3a58f1f4186786ee0299c7abb04ff86
vithonghcm <vithonghcm@gmail.com>
2016-12-15 10:54:04 +0700

    header change

--- public/js/header/script.js
+++ public/js/header/script.js
@@ -1,14 +1,23 @@
-/*
-$(document).ready(function() {
-	$('#search-open').on('click',function(event){
-		alert('aa');
-		//$("#g3c-search-container").toggleClass('g3c-search-open');
+$( document ).ready(function() {
+	$('#g3c-search-toogle-btn').on('click', function(){
+		$('#g3c-search-container').toggleClass('g3c-top0');
+		if(!$('#search-input').is(':focus')){
+			$('#search-input').focus();
+		}
+	});
+	$('#g3c-search-remove-btn').on('click', function(){
+		$('#g3c-search-container').removeClass('g3c-top0');
+		$('#search-input').blur();
 	});
 });
-*/
-$('body').on('click', '#search-open', function () { 
-	$("#g3c-search-container").toggleClass('g3c-search-open');
+
+$('body').on('click', '#g3c-search-toogle-btn', function () { 
+	$('#g3c-search-container').toggleClass('g3c-top0');
+	if(!$('#search-input').is(':focus')){
+		$('#search-input').focus();
+	}
 });
-	
-
-
+$('body').on('click', '#g3c-search-remove-btn', function () { 
+	$('#g3c-search-container').removeClass('g3c-top0');
+		$('#search-input').blur();
+});
